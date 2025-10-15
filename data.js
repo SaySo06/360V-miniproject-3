aa27d29684373b8f9821f9c87b2452a21c15b4d8
SaySo06 <miv28715gg@gmail.com>
2025-10-15 23:38:58 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,483 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-en16307",
+      "name": "EN16307",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.157660406055589,
+          "pitch": 0.03185846440994311,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hallway-",
+      "name": "Hallway หน้าแลปคอมโปร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.413040523655658,
+          "pitch": -0.10432925104996649,
+          "rotation": 0,
+          "target": "9-hallway"
+        },
+        {
+          "yaw": 1.5165363093451862,
+          "pitch": -0.10184715883565332,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_25681008_104612_309",
+      "name": "IMG_25681008_104612_309",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1155675154607803,
+          "pitch": 0.015337580181750354,
+          "rotation": 0,
+          "target": "3-img_25681008_104557_741"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-img_25681008_104557_741",
+      "name": "IMG_25681008_104557_741",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.023085126174136,
+          "pitch": 0.02711660846889785,
+          "rotation": 0,
+          "target": "2-img_25681008_104612_309"
+        },
+        {
+          "yaw": -1.2210473269227968,
+          "pitch": 0.08725703222673076,
+          "rotation": 0,
+          "target": "5-hallway-cd"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "ทางเข้าแลปคอมโปร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.808149761260216,
+          "pitch": -0.16352055967007573,
+          "rotation": 0,
+          "target": "6-lab-a"
+        },
+        {
+          "yaw": -2.8294828961349907,
+          "pitch": -0.05699638446400357,
+          "rotation": 0,
+          "target": "5-hallway-cd"
+        },
+        {
+          "yaw": -0.1836342057513729,
+          "pitch": -0.16940172282261123,
+          "rotation": 0,
+          "target": "1-hallway-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hallway-cd",
+      "name": "Hallway หน้าแลปCD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8112132510327914,
+          "pitch": -0.0626968850267744,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.3867961516508238,
+          "pitch": -0.22720868272671346,
+          "rotation": 0,
+          "target": "3-img_25681008_104557_741"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-lab-a",
+      "name": "Lab A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4227426371586418,
+          "pitch": 0.018657180653031347,
+          "rotation": 0,
+          "target": "7-lab-b"
+        },
+        {
+          "yaw": -0.19720829575015664,
+          "pitch": 0.00913641216836858,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-lab-b",
+      "name": "Lab B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0823162342652335,
+          "pitch": -0.0037935153651300624,
+          "rotation": 0,
+          "target": "6-lab-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "หน้าห้องประชุมใหญ่",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5536230592601328,
+          "pitch": -0.16397961646644532,
+          "rotation": 0,
+          "target": "0-en16307"
+        },
+        {
+          "yaw": 2.5360443558413177,
+          "pitch": -0.044653409207157324,
+          "rotation": 0,
+          "target": "9-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.811304960075775,
+        "pitch": 0.1628798001922096,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8807610644671655,
+          "pitch": -0.04501016124595836,
+          "rotation": 6.283185307179586,
+          "target": "10-"
+        },
+        {
+          "yaw": -1.3726762398152168,
+          "pitch": -0.0408586484748632,
+          "rotation": 0,
+          "target": "1-hallway-"
+        },
+        {
+          "yaw": 1.3285053097536235,
+          "pitch": -0.06375791493535843,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "หน้าลิฟต์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7142290034375556,
+          "pitch": -0.050385051891161936,
+          "rotation": 0,
+          "target": "9-hallway"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "เพียรวิจิตร 3rd floor 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
